0f583f18ed6747c8a87c7227125bfa400a5c46c5
Daichi Kanke <kanke@tam-tam.co.jp>
2017-10-08 02:58:25 +0900

    Todoのリスト部分のベース作成

---
+++ src/components/Todos.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <form>
+      <input type="text" placeholder="add New Task">
+      <button value="Add">Add</button>
+    </form>
+
+    <ul class="todos">
+      <li class="todo" v-for="todo in todos" :class="{ isFinished: todo.finished }">
+        <label><input type="checkbox" v-model="todo.finished"> {{todo.text}}</label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todos',
+  data () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'task1です',
+          finished: false
+        }, {
+          id: 2,
+          text: 'task2だよ',
+          finished: false
+        }, {
+          id: 3,
+          text: 'task3っすよ',
+          finished: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+
+.todo.isFinished {
+  text-decoration: line-through;
+  color: #999;
+}
+
+
+</style>
