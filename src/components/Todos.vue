822d858f5c6ed4d400858f4f46ed87c868aa549f
Daichi Kanke <kanke@tam-tam.co.jp>
2017-10-08 03:24:41 +0900

    タスクの追加処理を作成

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <form>
-      <input type="text" placeholder="add New Task">
-      <button value="Add">Add</button>
-    </form>
+    <div>
+      <input type="text" v-model="newTask.text" placeholder="add New Task">
+      <button value="Add" @click="addNewTask()">Add</button>
+    </div>
 
     <ul class="todos">
       <li class="todo" v-for="todo in todos" :class="{ isFinished: todo.finished }">
-        <label><input type="checkbox" v-model="todo.finished"> {{todo.text}}</label>
+        <label><input type="checkbox" v-model="todo.finished"> {{todo.id}} - {{todo.text}}</label>
       </li>
     </ul>
   </div>
@@ -32,7 +32,24 @@ export default {
           text: 'task3っすよ',
           finished: false
         }
-      ]
+      ],
+      newTask: {
+        text: ''
+      }
+    }
+  },
+  methods: {
+    addNewTask () {
+      if (this.newTask.text === '') return
+
+      var newTask = {
+        id: this.todos.length + 1,
+        text: this.newTask.text,
+        finished: false
+      }
+
+      this.todos.push(newTask)
+      this.newTask.text = ''
     }
   }
 }
