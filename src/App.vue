d44086fadf944843ccdc1165e4e617422f5847fa
Daichi Kanke <kanke@tam-tam.co.jp>
2017-10-08 02:57:41 +0900

    [ first Commit ]Vue-Cliで環境作成

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Vue ToDo Sample</h1>
+    <todos></todos>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+  padding: 30px;
+}
+</style>
